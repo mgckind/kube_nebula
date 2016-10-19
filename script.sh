76c537f523137881452645769876dcc122fdda97
Matias Carrasco Kind <mgckind@gmail.com>
2016-10-19 15:33:00 -0500

    update master yml

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-#sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $HOSTNAME/g" /etc/hosts
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
