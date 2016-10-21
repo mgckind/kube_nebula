4332c70f8de4470c5f92e8a35b9b93834f15dfea
Matias Carrasco Kind <mgckind@gmail.com>
2016-10-20 23:58:44 -0500

    delete script

--- script.sh
+++
@@ -1,4 +0,0 @@
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
